4f6f83356993cc656ea7a4ae12dd37af870c42ba
AsterWinston <3236223553@qq.com>
2025-09-17 13:30:01 +0800

    v1.0.9

--- src/main/java/com/aster/cloud/test/Test01.java
+++ src/main/java/com/aster/cloud/test/Test01.java
@@ -4,6 +4,7 @@ import com.aster.cloud.beans.FileOrDirInformation;
 import com.aster.cloud.utils.FileManager;
 import com.aster.cloud.utils.IPManager;
 import com.aster.cloud.utils.PathManager;
+import com.aster.cloud.utils.UUIDGenerator;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -14,11 +15,17 @@ import java.util.zip.ZipOutputStream;
 
 public class Test01 {
     public static void main(String[] args) throws IOException {
-        test01();
+//        test01();
+        test02();
     }
 
     private static void test01(){
         System.out.println(IPManager.isIPv4("111.111.111.11"));
     }
+    private static void test02() {
+        for (int i = 0; i < 10; i++){
+            System.out.println(UUIDGenerator.generateUniqueDirectoryName().length());
+        }
 
+    }
 }
